f235ae71cb434189f1f2cbdd8259d3add5b8e4cb
Tanmayi Manku <tanmayitanum@gmail.com>
2023-09-29 03:30:56 +0530

    add code of decoding

--- decode.rb
+++ decode.rb
@@ -28,3 +28,70 @@ end
 
 print "Sentence: \n"
 puts sen ('A BC DDD A')
+
+# Morse code message decoding code
+
+MORSE_CODE = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z',
+  '-----' => '0',
+  '.----' => '1',
+  '..---' => '2',
+  '...--' => '3',
+  '....-' => '4',
+  '.....' => '5',
+  '-....' => '6',
+  '--...' => '7',
+  '---..' => '8',
+  '----.' => '9'
+}.freeze
+
+def decode_char(morse_char)
+  MORSE_CODE[morse_char] || ''
+end
+
+# puts decode_char(".-") => A
+
+def decode_word(morse_word)
+  morse_code_to_words = morse_word.split
+  word = ''
+  morse_code_to_words.each do |i|
+    word += decode_char(i)
+  end
+  word
+end
+
+# puts decode_word("-- -.--") => MY
+
+def decode_message(morse_message)
+  morse_words = morse_message.split('   ')
+  decoded_words = morse_words.map { |word| decode_word(word) }
+  decoded_words.join(' ')
+end
+
+# puts decode_message(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+# output: A BOX FULL OF RUBIES
